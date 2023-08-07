c2d958f5ab500e23fb85d3f47129857f155726a7
Ash <ashfaqur.rahman1@gmail.com>
2023-08-07 15:08:27 +0200

    analyze log script (#5)

---
+++ src/analyze_log.py
@@ -0,0 +1,73 @@
+import argparse
+import json
+import logging
+import os
+from typing import Tuple
+
+logger = logging.getLogger(__name__)
+
+
+def analyze_log_file(log_file_path: str, triage_file_path: str) -> Tuple[str, str]:
+    logger.debug(f"Given log file path: {log_file_path}")
+    check_valid_file(log_file_path)
+    logger.debug(f"Given triage file path: {triage_file_path}")
+    check_valid_file(triage_file_path)
+    logs: list[str] = load_logs(log_file_path)
+    triages: dict[str, str] = load_triages(triage_file_path)
+    analysis: Tuple[str, str] = look_for_error(logs, triages)
+    return analysis
+
+
+def look_for_error(logs: list[str], triages: dict[str, str]) -> Tuple[str, str]:
+    for line in logs:
+        for k, v in triages.items():
+            if k in line:
+                logger.info("------------------------------------------------")
+                logger.info(f"   ERROR FOUND:   {line}")
+                logger.info(f"   RESOLUTION:    {v}")
+                logger.info("------------------------------------------------")
+                return k, v
+    return "", ""
+
+
+def check_valid_file(given_file_path: str) -> None:
+    if not os.path.isfile(given_file_path):
+        raise ValueError(
+            f"{given_file_path} is not a valid file path or the file does not exist on disk"
+        )
+
+
+def load_logs(log_file_path: str) -> list[str]:
+    with open(log_file_path, "r") as f:
+        lines: list[str] = [line.rstrip() for line in f]
+        return lines
+
+
+def load_triages(triage_file_path: str) -> dict[str, str]:
+    try:
+        with open("tests/assets/sample_triage_file.json", "r") as f:
+            triages: dict[str, str] = json.load(f)
+            return triages
+    except json.JSONDecodeError as e:
+        raise Exception(f"Invalid JSON file: {e}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Analyze log file")
+    parser.add_argument("path", type=str, help="Path to a log file")
+    parser.add_argument("triage_file_path", type=str, help="a path to another file")
+    parser.add_argument(
+        "-v", "--verbose", action="store_true", help="Set the log level to DEBUG"
+    )
+
+    args = parser.parse_args()
+    log_level = logging.INFO
+    # Set the log level
+    if args.verbose:
+        log_level = logging.DEBUG
+
+    logging.basicConfig(level=log_level)
+
+    log_file: str = args.path
+    triage_file: str = args.triage_file_path
+    analyze_log_file(log_file, triage_file)
