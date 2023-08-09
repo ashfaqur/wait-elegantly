b0d5be80322dcad060cf7539d5abc271659812b3
Ashfaqur Rahman <ashfaqur.rahman1@gmail.com>
2023-08-09 12:25:18 +0200

    running commands

--- src/command.py
+++ src/command.py
@@ -1,4 +1,12 @@
+import subprocess
+import time
+from datetime import datetime
+from pathlib import Path
 from typing import Any
+
+import progressbar
+
+from analyze_log import analyze_log_file
 
 COMMANDS_KEY = "commands"
 COMMAND_NAME = "name"
@@ -14,3 +22,60 @@ class Command:
         print(self.name)
         print(self.id)
         print(self.values)
+
+    def run(self, triage_file: str) -> None:
+        log_dir = Path("build/log")
+        log_dir.mkdir(parents=True, exist_ok=True)
+
+        history = Path(f"build/history/{self.id}")
+        history.parent.mkdir(parents=True, exist_ok=True)
+        history.touch()
+
+        with open(history, "r") as f:
+            numbers: list[int] = [int(line.strip()) for line in f]
+
+        if numbers:
+            expected_time = sum(numbers) / len(numbers)
+            print(f"Expected time to complete: {expected_time}s")
+        else:
+            expected_time = 0
+
+        now = datetime.now()
+        formatted_timestamp = now.strftime("%H-%M-%S_%d-%m-%Y")
+        file_name = f"{self.id}_{formatted_timestamp}.txt"
+        with open(log_dir / file_name, "w") as log_file:
+            start_time = time.time()
+            process = subprocess.Popen(
+                self.values, stdout=log_file, stderr=log_file, universal_newlines=True
+            )
+
+            bar = progressbar.ProgressBar(
+                widgets=[
+                    progressbar.Percentage(),
+                    " ",
+                    progressbar.Bar(),
+                    " ",
+                    progressbar.ETA(),
+                ],
+                max_value=expected_time,
+            )
+            bar.start()
+            while process.poll() is None:
+                time.sleep(1)
+                if bar.value < bar.max_value:
+                    bar.update(1)
+            total_time = round(time.time() - start_time)
+            bar.finish()
+
+            with history.open("a") as f:
+                f.write(f"{total_time}\n")
+
+            difference = round(total_time - expected_time)
+            if difference > 0:
+                print(f"Took {abs(difference)}s MORE than expected")
+            else:
+                print(f"Took {abs(difference)}s less than expected")
+
+            if process.returncode == 0:
+                print("FAILED")
+                analyze_log_file(log_dir / file_name, triage_file)
--- src/wait_elegantly.py
+++ src/wait_elegantly.py
@@ -7,12 +7,11 @@ from command import COMMANDS_KEY
 from command import Command
 
 
-def wait_elegantly(config: str) -> None:
+def wait_elegantly(config: str, triage_file: str) -> None:
     with open(config, "r") as f:
         data = yaml.safe_load(f)
 
-    commands = [Command(command) for command in data[COMMANDS_KEY]]
-    print(commands)
+    [Command(command).run(triage_file) for command in data[COMMANDS_KEY]]
 
 
 if __name__ == "__main__":
@@ -20,6 +19,7 @@ if __name__ == "__main__":
         description="Wait Elegantly while commands executes"
     )
     parser.add_argument("path", type=str, help="Path to a config yaml file")
+    parser.add_argument("triage", type=str, help="Path to a triage error json file")
     parser.add_argument(
         "-v", "--verbose", action="store_true", help="Set the log level to DEBUG"
     )
@@ -32,5 +32,6 @@ if __name__ == "__main__":
     logging.basicConfig(level=log_level)
 
     config_file: str = args.path
+    triage_file: str = args.triage
 
-    wait_elegantly(config_file)
+    wait_elegantly(config_file, triage_file)
